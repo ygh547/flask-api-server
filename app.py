92c0edff61d8c21abcd226aaf2825a4c91958545
ygh547 <ygh547@naver.com>
2022-06-17 11:56:53 +0900

    add

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # API가 있어야 한다. 아래코드가 API
 @app.route('/' , mothods= ['GET'])
 def hello_world():
-    return 'Hello World'
+    return 'Hello World hihihi'
 
 if __name__ == '__main__' :
     app.run()
